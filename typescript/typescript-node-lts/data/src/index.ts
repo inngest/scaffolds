304d1d03d767d74c77e576a821ec5eff9c70723c
Tony Holdstock-Brown <tonyhb@gmail.com>
2022-03-10 16:44:37 -0500

    Remove action from destructure.

--- typescript/typescript-node-lts/data/src/index.ts
+++ typescript/typescript-node-lts/data/src/index.ts
@@ -1,6 +1,6 @@
 import type { Args } from "./types";
 
-export async function run({ event, actions }: Args) {
+export async function run({ event }: Args) {
   // Your logic goes here.
   return event.name;
 }
