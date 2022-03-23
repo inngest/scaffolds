bbad6fca8c9ccb0f821e01ef1f44504fd27cf712
Tony Holdstock-Brown <tonyhb@gmail.com>
2022-03-22 21:56:29 -0400

    Change dockerfile, _init

--- typescript/typescript-node-lts/data/src/_init.ts
+++ typescript/typescript-node-lts/data/src/_init.ts
@@ -1,12 +1,23 @@
-import type { Args } from "./types";
+import type { EventTriggers } from "./types";
 import { run } from "./index";
+
+type Context = {
+  baggage: {
+    WorkspaceEvent: {
+      Event: EventTriggers;
+    },
+    Actions: {
+      [clientID: string]: any
+    }
+  }
+}
 
 /**
  * Init initializes the context for running the function.  This calls
  * start() when
  */
 async function init() {
-  let context: Args | undefined;
+  let context: Context | undefined;
 
   // We pass the event in as an argument to the node function.  Running
   // npx ts-node "./foo.bar" means we have 2 arguments prior to the event.
@@ -17,13 +28,17 @@ async function init() {
     throw new Error("unable to parse context");
   }
 
-  const result = await run(context);
+  const result = await run({ event: context.baggage.WorkspaceEvent.Event, actions: context.baggage.Actions });
   return result;
 }
 
 init()
   .then(result => {
     // TODO If this isn't an object, wrap this in a result object.
+    if (typeof result === "string") {
+      console.log(JSON.stringify({ result }));
+      return;
+    }
     console.log(JSON.stringify(result))
   })
   .catch(e => {
